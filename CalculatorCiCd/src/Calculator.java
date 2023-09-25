9bb85f6d429d0a3debcf4e498f5f5df8625dac2c
Nelson Linares <nelsonlin107@gmail.com>
2023-09-24 22:31:58 -0300

    Adding advanced methods

--- CalculatorCiCd/src/Calculator.java
+++ CalculatorCiCd/src/Calculator.java
@@ -1,4 +1,15 @@
 
 public class Calculator {
 
+	public int multiply(int a, int b) {
+		return a*b;
+	}
+	
+	public double division(int a, int b) {
+		if (b==0) {
+			throw Arithmetic Exception("No se puede dividir entre 0");
+		}
+		return (double) a/b;
+	}
+	
 }
